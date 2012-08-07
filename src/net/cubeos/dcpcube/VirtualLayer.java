b28695794443e4b8b908968437fa56749ce37d68
SashaCrofter <sasha@crofter.org>
2012-08-06 20:53:55 -0400

    initialized the project improved the .gitignore file

---
+++ src/net/cubeos/dcpcube/VirtualLayer.java
@@ -0,0 +1,20 @@
+package net.cubeos.dcpcube;
+
+/**
+ * This is the virtual layer for DCPcUbe, which is responsible
+ * for loading the proper binary, and arranging that the CPU
+ * execute it
+ */
+public class VirtualLayer
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
